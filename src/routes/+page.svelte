7844b8e816cc54cd36a49cee62f06b9f36b8ae07
G2p1 <andrii.tiuitunnyk@gmail.com>
2023-07-07 14:59:17 +0300

    feat(#1): Add button upploading and submit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<div class="rows">
+    <div class="row">
+        <input class="button is-outlined" 
+            multiple 
+            type="file" 
+            name="media_uploads" 
+            id="media_uploads"
+            accept=".jpg,
+                    .gif,
+                    .png,
+                    .webp,
+                    .x-msvideo,
+                    .h264,
+                    .x-m4v,
+                    .mpeg,
+                    .mp4,
+                    .ogg,
+                    .webm">
+   
+        <button class="button is-primary is-outlined">Send</button>
+    </div>
+</div>
+
+<style>
+
+</style>
