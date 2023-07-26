ada3dea3870a6efefeb56072ef4ec5e997ad8a85
G2p1 <andrii.tiuitunnyk@gmail.com>
2023-07-26 22:45:19 +0300

    feat(#2): Add submit and pick fiile button
    Signed-off-by: G2p1 <andrii.tiuitunnyk@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,36 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<section class="section">
+  <div class="container">
+      <div class="box">
+          <div class="rows is-centered">
+              
+              <div class="row has-text-centered">
+                  <input class="button is-outlined" 
+                          multiple 
+                          type="file" 
+                          name="media_uploads" 
+                          id="media_uploads"
+                          accept=".jpg,
+                          .gif,
+                          .png,
+                          .webp,
+                          .x-msvideo,
+                          .h264,
+                          .x-m4v,
+                          .mpeg,
+                          .mp4,
+                          .ogg,
+                          .webm">
+                  <button class="button is-primary is-outlined">Send</button>
+              </div>
+          </div>
+      </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+</section>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
